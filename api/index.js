c69985fdb19b224073983dece84ee50a02014cd5
Chanuth10 <chanuthmaduka1986@gmail.com>
2024-04-10 12:00:00 +0530

    db connect

---
+++ api/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: "",
+    credentials: true
+}))
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  app.use("/", (req, res) => {
+    res.send("Book Store Server is running!");
+  });
+}
+
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
